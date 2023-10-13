a85847da358d4b2fc9265addd51084635b73bb10
Iker <iker_hj@outlook.com>
2023-10-13 11:27:11 +0200

    ADDED: half adder test bench

---
+++ ycbcr.project/tb/half_adder.vhd
@@ -0,0 +1,70 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity half_adder_tb is
+end entity;
+
+architecture tb of half_adder_tb is
+component half_adder is
+Port ( 
+	ha, hb : in STD_LOGIC;
+	hs, hcout : out STD_LOGIC
+	);
+end component;
+
+signal ha : std_logic:='0';
+signal hb : std_logic:='0';
+signal fcin : std_logic:='0';
+signal hs, fcout : STD_LOGIC;
+
+begin
+ 
+uut : half_adder port map(
+	ha =>ha, 
+	hb =>hb,
+	hs => hs, 
+	fcout => fcout
+	);
+
+ -- Stimulus process
+ stim_proc: process
+ begin
+ -- hold reset state for 100 ns.
+ wait for 100 ns;
+
+
+ha <= '0';
+hb <= '0';
+wait for 10 ns;
+
+ha <= '0';
+hb <= '0';
+wait for 10 ns;
+
+ha <= '0';
+hb <= '1';
+wait for 10 ns;
+
+ha <= '0';
+hb <= '1';
+wait for 10 ns;
+
+ha <= '1';
+hb <= '0';
+wait for 10 ns;
+
+ha <= '1';
+hb <= '0';
+wait for 10 ns;
+
+ha <= '1';
+hb <= '1';
+wait for 10 ns;
+
+ha <= '1';
+hb <= '1';
+wait for 10 ns;
+wait;
+
+end process;
+end tb;
