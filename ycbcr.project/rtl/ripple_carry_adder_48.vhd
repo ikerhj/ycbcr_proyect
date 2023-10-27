f2af150b23d35c81283e4e7f38736bd8af2615f1
Iker <iker_hj@outlook.com>
2023-10-27 09:14:49 +0200

    FIXED: name of entity from 24 to 48

--- ycbcr.project/rtl/ripple_carry_adder_48.vhd
+++ ycbcr.project/rtl/ripple_carry_adder_48.vhd
@@ -1,15 +1,15 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
  
-entity ripple_carry_adder_24 is
+entity ripple_carry_adder_48 is
 Port ( A : in STD_LOGIC_VECTOR (47 downto 0);
 B : in STD_LOGIC_VECTOR (47 downto 0);
 Cin : in STD_LOGIC;
 S : out STD_LOGIC_VECTOR (47 downto 0);
 Cout : out STD_LOGIC);
-end ripple_carry_adder_24;
+end ripple_carry_adder_48;
  
-architecture Behavioral of ripple_carry_adder_24 is
+architecture Behavioral of ripple_carry_adder_48 is
  
 -- Full Adder VHDL Code Component Decalaration
 component ripple_carry_adder_24
