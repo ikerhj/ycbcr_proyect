8acf004c9da3fb6420450947bdbc581c9bf60673
Iker <iker_hj@outlook.com>
2023-11-02 18:24:59 +0100

    FIX: use the shift_left numeric_std function

--- ycbcr.project/rtl/unsigned_multiplier_24.vhd
+++ ycbcr.project/rtl/unsigned_multiplier_24.vhd
@@ -2,20 +2,27 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity braun_multiplier is
+entity unsigned_multiplier_24 is
     port (
         a : in std_logic_vector(23 downto 0);
         b : in std_logic_vector(23 downto 0);
         c : out std_logic_vector(47 downto 0)
     );
-end braun_multiplier;
+end unsigned_multiplier_24;
 
-architecture Behavioral of braun_multiplier is
+architecture Behavioral of unsigned_multiplier_24 is
     signal partial_products : std_logic_vector(47 downto 0);
     signal sum : std_logic_vector(47 downto 0);
 begin
     gen_partial_products: for i in 0 to 23 generate
-        partial_products(2*i downto 2*i) <= std_logic_vector(shift_left(unsigned(a), i) when b(i) = '1' else (others => '0'));
+        process(b)
+        begin
+            if b(i) = '1' then
+                partial_products(2*i downto 2*i) <= std_logic_vector(shift_left(unsigned(a), i));
+            else
+                partial_products(2*i downto 2*i) <= (others => '0');
+            end if;
+        end process;
     end generate gen_partial_products;
 
     gen_sum: for i in 0 to 47 generate
