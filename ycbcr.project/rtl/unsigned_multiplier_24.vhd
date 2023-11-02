0f4036b8df0c4299b6500ce1ed22be37441cc8b0
Iker <iker_hj@outlook.com>
2023-11-02 19:24:24 +0100

    ADDED: clk logic

--- ycbcr.project/rtl/unsigned_multiplier_24.vhd
+++ ycbcr.project/rtl/unsigned_multiplier_24.vhd
@@ -7,7 +7,8 @@ entity unsigned_multiplier_24 is
   port (
     a : in  std_logic_vector(23 downto 0);   -- low-active reset
     b : in  std_logic_vector(23 downto 0);
-    c : out std_logic_vector(47 downto 0)
+    c : out std_logic_vector(47 downto 0);
+    clk : in std_logic
   );
 end entity unsigned_multiplier_24;
 
@@ -24,17 +25,19 @@ begin
     -- Use shifted_a as an actual parameter in the component instantiation
 
     -- Remove the sensitivity list and use a wait statement instead
-    process (a, b) is
+    process (clk) is  -- assuming 'clk' is your clock signal
     begin
-        a_48bit <= (47 downto a'length => '0') & a;
-        b_48bit <= (47 downto b'length => '0') & b;
-        temp <= (others => '0');
-        for i in 0 to 47 loop
-            if b_48bit(i) = '1' then
-                temp <= std_logic_vector(unsigned(temp) + shift_left(unsigned(a_48bit), i));
-            end if;
-        end loop;
-        c <= temp;
+        if rising_edge(clk) then  -- process executes on the rising edge of the clock
+            a_48bit <= (47 downto a'length => '0') & a;
+            b_48bit <= (47 downto b'length => '0') & b;
+            temp <= (others => '0');
+            for i in 0 to 47 loop
+                if b_48bit(i) = '1' then
+                    temp <= std_logic_vector(unsigned(temp) + shift_left(unsigned(a_48bit), i));
+                end if;
+            end loop;
+            c <= temp;
+        end if;
     end process;
 end Behavioral;
 
