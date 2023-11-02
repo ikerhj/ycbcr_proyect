0372daf760478e5da82fa6c06732b0fbf9bb04db
Iker <iker_hj@outlook.com>
2023-11-02 22:08:44 +0100

    REMOVED: clk logic from tb

--- ycbcr.project/tb/unsigned_multiplier_24_tb.vhd
+++ ycbcr.project/tb/unsigned_multiplier_24_tb.vhd
@@ -8,7 +8,6 @@ end entity unsigned_multiplier_24_tb;
 architecture test of unsigned_multiplier_24_tb is
     component unsigned_multiplier_24 is
         port (
-            clk : in std_logic;  -- add clock signal
             a : in std_logic_vector(23 downto 0);
             b : in std_logic_vector(23 downto 0);
             c : out std_logic_vector(47 downto 0)
@@ -17,50 +16,43 @@ architecture test of unsigned_multiplier_24_tb is
 
     signal a, b : std_logic_vector(23 downto 0);
     signal c : std_logic_vector(47 downto 0);
-    signal clk : std_logic := '0';  -- add clock signal
 begin
-    -- clock process
-    clk_process : process
-    begin
-        wait for 50 ns;  -- adjust the time period as needed
-        clk <= not clk;
-    end process;
 
 
     uut : unsigned_multiplier_24 port map (clk => clk, a => a, b => b, c => c);  -- connect clock signal
 
     stimulus : process
     begin
-        wait until rising_edge(clk);
+        wait for 100ns;
         a <= x"000000";
         b <= x"000000";
-        wait until rising_edge(clk);
+        wait for 100ns;
         assert c = x"000000000000" report "Test Case 1 Failed" severity error;
 
         -- repeat for other test cases...
         a <= x"000000";
         b <= x"000000";
-        wait until rising_edge(clk);
+        wait for 100ns;
         assert c = x"000000000000" report "Test Case 1 Failed" severity error;
 
         a <= x"000001";
         b <= x"000001";
-        wait until rising_edge(clk);
+        wait for 100ns;
         assert c = x"000001000001" report "Test Case 2 Failed" severity error;
 
         a <= x"0000FF";
         b <= x"0000FF";
-        wait until rising_edge(clk);
+        wait for 100ns;
         assert c = x"0000FE01FF" report "Test Case 3 Failed" severity error;
 
         a <= x"7FFFFF";
         b <= x"7FFFFF";
-        wait until rising_edge(clk);
+        wait for 100ns;
         assert c = x"3FFFFE000001" report "Test Case 4 Failed" severity error;
 
         a <= x"FFFFFF";
         b <= x"FFFFFF";
-        wait until rising_edge(clk);
+        wait for 100ns;
         assert c = x"FE0000010001" report "Test Case 5 Failed" severity error;
         wait;
     end process stimulus;
