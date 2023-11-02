b2f78dc3c67877ec25b965e19cda241da59af2d4
Iker <iker_hj@outlook.com>
2023-11-02 18:38:03 +0100

    CHANGES: to the braun method

--- ycbcr.project/rtl/unsigned_multiplier_24.vhd
+++ ycbcr.project/rtl/unsigned_multiplier_24.vhd
@@ -1,33 +1,37 @@
-library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity unsigned_multiplier_24 is
+entity unsigned_braun_multiplier_24 is
     port (
         a : in std_logic_vector(23 downto 0);
         b : in std_logic_vector(23 downto 0);
         c : out std_logic_vector(47 downto 0)
     );
-end unsigned_multiplier_24;
+end unsigned_braun_multiplier_24;
 
-architecture Behavioral of unsigned_multiplier_24 is
-    signal partial_products : std_logic_vector(47 downto 0);
-    signal sum : std_logic_vector(47 downto 0);
+architecture Behavioral of unsigned_braun_multiplier_24 is
+    type array_2d is array (23 downto 0, 23 downto 0) of std_logic_vector(47 downto 0);
+    signal partial_products : array_2d;
+    signal sum : array_2d;
 begin
     gen_partial_products: for i in 0 to 23 generate
-        process(b)
-        begin
-            if b(i) = '1' then
-                partial_products(2*i downto 2*i) <= std_logic_vector(shift_left(unsigned(a), i));
-            else
-                partial_products(2*i downto 2*i) <= (others => '0');
-            end if;
-        end process;
+        for j in 0 to 23 generate
+            process(b)
+            begin
+                if b(j) = '1' then
+                    partial_products(i, j) <= std_logic_vector(shift_left(unsigned(a), i));
+                else
+                    partial_products(i, j) <= (others => '0');
+                end if;
+            end process;
+        end generate;
     end generate gen_partial_products;
 
-    gen_sum: for i in 0 to 46 generate
-        sum(i) <= partial_products(i) or partial_products(i+1);
+    gen_sum: for i in 0 to 23 generate
+        for j in 0 to 23-i generate
+            sum(i, j) <= partial_products(i, j) + partial_products(i, j+1);
+        end generate;
     end generate gen_sum;
 
-    c <= sum;
+    c <= sum(0, 0);
 end Behavioral;
